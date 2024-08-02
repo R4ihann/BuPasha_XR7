62fffc1c2340eb3bdf9ef0e470aeac31dd174383
Faatihah Ahmad Raihan <faatihah_ilmihi_33rpl@student.smktelkom-mlg.sch.id>
2024-08-02 13:53:43 +0700

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,6 @@
+public class Test {
+    public static void main(String[] arStrings) {
+        System.out.println("Halo nama saya Raihan dari kelas X-RPL7 yang berasal dari kota Cirebon");
+        String a = "1";
+    }
+}
