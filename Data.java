d3b19df89648773a377a0da803be9599aab3da00
Faatihah Ahmad Raihan <faatihah_ilmihi_33rpl@student.smktelkom-mlg.sch.id>
2024-08-05 11:36:18 +0700

    Add files via upload

---
+++ Data.java
@@ -0,0 +1,13 @@
+public class Data {
+    public static void main(String[] args) {
+        String name = "Raihan"; //this is a string 
+        int age = 16;
+        String Address = "Jl. samadikun Gang Empang 5, No.14";
+        double Class = 10.7;
+
+        System.out.println("my name is "+ name);
+        System.out.println("i am "+ age + " years old");
+        System.out.println("My address is in "+ Address);
+        System.out.println("my class is in "+ Class);
+    }
+}
