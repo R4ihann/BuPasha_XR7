bdeb036fba8822b681a569ef31cc578efb43c40e
Faatihah Ahmad Raihan <faatihah_ilmihi_33rpl@student.smktelkom-mlg.sch.id>
2024-08-23 13:51:34 +0700

    Add files via upload

--- Karyawantester.java
+++ Karyawantester.java
@@ -1,6 +1,19 @@
 //object class
+import java.util.Scanner;
 public class Karyawantester {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Inputkan nama Siswa: ");
+        String nama = input.nextLine();
+        System.out.println("Inputkan ID Siswa: ");
+        int id = input.nextInt();
+        System.out.println("inputkan IPK Siswa: ");
+        double ipk = input.nextDouble();
+
+        Siswa nimas = new Siswa(id, nama, ipk);
+        nimas.print();
+        input.close();
+
         // object
         // Class object = new Construction
         Karyawan Willy = new Karyawan(35,"Willy","TU","Madura");
